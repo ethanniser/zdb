9afeba49606447a2607c08d514c46f4a03f04f32
Ethan Niser <ethan@niser.com>
2024-10-15 05:13:34 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nvim.url = "github:ethanniser/nvim.nix";
+    nvim.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, nvim, ... }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [nvim.overlays.default];
+    };
+  in {
+    devShells = {
+      x86_64-linux = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            zig
+            zls
+            configured-nvim
+          ];
+        };
+      };
+    };
+  };
+}
+
